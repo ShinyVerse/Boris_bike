18ee1188ccfc4530c468fdf4ae0e16b5a785932f
Laura Jackson <playeronereadystart@gmail.com>
2018-09-05 17:51:01 +0100

    Complete up to Challenge 17

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,14 +1,35 @@
+require "./lib/bike.rb"
+
 class DockingStation
+
+  DEFAULT_CAPACITY = 20
+  attr_reader :bike_rack
+
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @capacity = capacity
+    @bike_rack = []
+  end
+
   def release_bike
-    bike = Bike.new()
+    raise "No more bikes left" if empty?
+    bike = Bike.new
   end
-
-  def working?
-  end
-
 
   def dock_bike(object)
-    array_of_bikes = []
-    array_of_bikes.push(object)
+    raise "Sorry, the bike rack is full" if full?
+    bike_rack.push(object)
+  end
+
+  def see_bikes
+    bike_rack
+  end
+
+  private
+  def full?
+    bike_rack.count >= @capacity
+  end
+
+  def empty?
+    bike_rack.count < 1
   end
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,19 +1,51 @@
 require 'dockingstation'
 
 describe DockingStation do
-  it 'responds to method call release_bike' do
-    expect(DockingStation.new()).to respond_to(:release_bike)
-  end
-  it 'responds to method call working?' do
-    expect(DockingStation.new()).to respond_to(:working?)
-  end
+  bike = Bike.new
+  station = DockingStation.new
+  station.dock_bike(bike)
+
+  it { is_expected.to respond_to(:release_bike)}
+
   it 'creates a new bike object when calling release_bike' do
-    expect(DockingStation.new().release_bike).to be_a_kind_of(Bike)
+    expect(station.release_bike).to an_instance_of(Bike)
   end
+
   it 'checks that the bike is working?' do
-    expect(DockingStation.new().release_bike.working?).to eq true
+    expect(station.release_bike.working?).to eq true
   end
+
   it 'responds to method called dock_bike' do
-    expect(DockingStation.new().dock_bike(Bike.new)).to be_a_kind_of(Array)
+    expect(subject.dock_bike(bike)).to an_instance_of(Array)
   end
+
+  it { is_expected.to respond_to(:see_bikes)}
+
+  it 'shows a bike if there is one.' do
+    expect(station.see_bikes[0]).to eq(bike)
+  end
+
+  it "raise error if there are no more bikes" do
+    expect {subject.release_bike}.to raise_error("No more bikes left")
+  end
+
+  it 'DockingStation has DEFAULT_CAPACITY constant' do
+    expect(DockingStation::DEFAULT_CAPACITY).to an_instance_of(Integer)
+  end
+
+  it "raises an error if there is no more space on the rack" do
+    expect {DockingStation::DEFAULT_CAPACITY.times {station.dock_bike(Bike.new)} }.to raise_error("Sorry, the bike rack is full")
+  end
+
+  it "user can overide capacity " do
+    new_station = DockingStation.new(30)
+    expect {25.times {new_station.dock_bike(Bike.new)}}.not_to raise_error
+  end
+
+  it "if capacity not given, DEFAULT_CAPACITY is used" do
+    new_station = DockingStation.new
+    expect {DockingStation::DEFAULT_CAPACITY.times {new_station.dock_bike(Bike.new)}}.not_to raise_error
+    expect {1.times {new_station.dock_bike(Bike.new)}}.to raise_error("Sorry, the bike rack is full")
+  end
+
 end
